0e75a4187a594e76eb6a27f3d4eb8fb9aaf14556
Kerwin McMillian <marink128@gmail.com>
2020-06-22 22:50:37 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 # Build your say_hello method here
-say_hello = "Ruby Programmer!"
+say_hello = "Kent Beck!"
 
-def say_hello ("Kent Beck!")
+def say_hello (name = "Ruby Programmer!")
   puts "Hello "
 end
 
